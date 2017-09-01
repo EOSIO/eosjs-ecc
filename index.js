42ecabb49e839b2891bf0b31706da85f9548bc64
James Calfee <james@jcalfee.info>
2017-09-01 14:57:17 -0500

    Adds es2015 npm publish command.

--- index.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    Aes: require("./src/aes"),
-    PrivateKey: require("./src/key_private"),
-    PublicKey: require("./src/key_public"),
-    Signature: require("./src/signature"),
-    key_utils: require("./src/key_utils"),
-    config: require('./src/config')
-}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    Aes: require("./aes"),
+    PrivateKey: require("./key_private"),
+    PublicKey: require("./key_public"),
+    Signature: require("./signature"),
+    key_utils: require("./key_utils"),
+    config: require('./config')
+}
