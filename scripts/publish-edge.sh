802197f76ee845a296880617b314a332c949e112
Nelson Ramirez <nelson.ramirez@block.one>
2019-07-03 11:30:42 -0400

    install npm

---
+++ scripts/publish-edge.sh
@@ -0,0 +1,28 @@
+echo "Starting publish edge" && \
+
+. "${TRAVIS_BUILD_DIR}/scripts/publish-utils.sh" && \
+
+echo "Running on branch/tag ${TRAVIS_BRANCH}" && \
+
+echo "Setting up git" && \
+setup_git && \
+
+echo "Creating new version" && \
+git checkout -- . && git status && \
+
+# # get the short commit hash to include in the npm package
+current_commit="$(git rev-parse --short HEAD)" && \
+
+npm install && \
+
+npm version prerelease -preid "${current_commit}" -no-git-tag-version && \
+
+git commit -a -m "Updating version [skip ci]" && \
+
+echo "Publish to NPM" && \
+
+cp .npmrc.template $HOME/.npmrc && \
+
+npm publish --tag edge && \
+
+echo "finished publish to edge"
